db7f33198335d90f64df1620a25b783306e7dbf1
Oliver Saxon <oliverrsaxon@gmail.com>
2021-05-11 11:46:06 +0100

    DB: Schema file created and database initiated in SQL workbench

---
+++ schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS cms_DB;
+CREATE DATABASE cms_DB;
+
+USE cms_DB;
+
+CREATE TABLE departments(
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE roles(
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL (10, 2) NOT NULL,
+    department_id INTEGER NOT NULL
+)
+
+CREATE TABLE employee(
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER
+)
