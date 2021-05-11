9b7ded99ef22be560ad0474c3d3387562775b072
Oliver Saxon <oliverrsaxon@gmail.com>
2021-05-11 13:24:50 +0100

    FEAT: Initial inquirer questions and actions defined

--- cms.js
+++ cms.js
@@ -1,34 +1,54 @@
-const mysql = require('mysql');
-const inquirer = require('inquirer');
+const mysql = require("mysql");
+const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: "localhost",
 
   port: 3306,
 
-  user: 'root',
+  user: "root",
 
-
-  password: 'google21',
-  database: 'cms_DB',
+  password: "google21",
+  database: "cms_DB",
 });
 
 connection.connect((err) => {
-    if (err) throw err;
-    console.log(`Welcome to the Employee Tracker CMS...`)
-    run();
-  });
+  if (err) throw err;
+  console.log(`Welcome to the Employee Tracker CMS...`);
+  run();
+});
 
 const run = () => {
-    inquirer.prompt({
-        name: 'action',
-        type: 'list',
-        message: 'What would you like to do?',
-        choices: [
-            'Add a new record',
-            'View records',
-            'Update an existing record',
-            'exit',
-        ]
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: ["Add a new record", "View records", "Update a record", "exit"],
     })
-}
+    .then((anser) => {
+      switch (answer.action) {
+        case "Add a new record":
+          addNewRecord();
+          break;
+
+        case "Update a record":
+          updateRecord();
+          break;
+
+        case "View records":
+            viewRecord();
+            break;
+        
+        case "exit":
+            connection.end();
+            break;    
+      }
+    });
+};
+
+const addNewRecord = () => {};
+
+const updateRecord = () => {};
+
+const viewRecord = () => {};
